e75b57916c8e3de2a28a548a4de71f4ae6044a44
Vinicius Morgado <vinicius.morgado@bsource.com.br>
2022-07-23 04:38:29 -0300

    Add orders resource

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('products')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('oneProduct')
+  getName(): string {
+    return 'Produtos';
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OrdersController } from './orders/orders.controller';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [],
+  imports: [OrdersModule],
   controllers: [AppController],
   providers: [AppService],
 })
